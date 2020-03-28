91825d6ec6f6e7054c0800bf3a737c620ceeda2c
chenlinqiang <327371999@qq.com>
2020-03-28 14:07:47 +0800

    struts begin

---
+++ eclipse-workspace/DemoLogin/WebContent/WEB-INF/login.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="/DemoLogin/login.do">
+    u: <input type="text" name="username" id=""/><br>
+    p: <input type="password" name="password" id=""/><br>
+    <input type="submit" name="" value="login" id="">
+</form>
+</body>
+</html>
---
+++ eclipse-workspace/DemoLogin/src/com/spe/actions/LoginAction.java
@@ -0,0 +1,31 @@
+package com.spe.actions;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import com.spe.forms.UserForm;
+
+public class LoginAction extends Action {
+
+	@Override
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		
+		UserForm userForm=(UserForm)form;
+		if("123".equals(userForm.getPassword())) {
+			return mapping.findForward("ok");
+		}else {
+			return mapping.findForward("err");
+			
+		}
+		
+	
+	}
+
+}
